3e1da0a4b4290c65e84715780b9507b5d54de4ae
Biola <abiola2326@gmail.com>
2020-04-30 04:47:44 +0100

    p5.1- pa5.2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,102 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('LoggedBloglistappUser')
+
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const changeHandler = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const passChangeHandler = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      window.localStorage.setItem('LoggedBloglistappUser', JSON.stringify(user))
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('invalid login')
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('LoggedBloglistappUser')
+    setUser(null)
+  }
+
+  const loginFormContainer = () => (
+    <div>
+      <h1>Login into the application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          Username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={changeHandler}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={passChangeHandler}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  if (user === null) {
+    return loginFormContainer()
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </p>
+
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
   )
 }
