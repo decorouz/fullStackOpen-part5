6b9957b071be90fae7e25b998ff10bcfe0bd480f
Biola <abiola2326@gmail.com>
2020-04-30 22:15:35 +0100

    p5.3 add new blog functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import Form from './components/Form'
-
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -9,6 +8,15 @@ const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  // const [newTitle, setNewTitle] = useState('')
+  // const [author, setNewAuthor] = useState('')
+  // const [url, seturl] = useState('')
+  const [form, setState] = useState({
+    title: '',
+    author: '',
+    url: '',
+  })
+
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -31,6 +39,29 @@ const App = () => {
 
   const passChangeHandler = (event) => {
     setPassword(event.target.value)
+  }
+
+  const updateAddBlogField = (event) => {
+    setState({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    const { title, author, url } = form
+
+    const blogObject = {
+      title,
+      author,
+      url,
+    }
+
+    const savedBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(savedBlog))
+
+    setState({ ...form, title: '', author: '', url: '' })
   }
 
   const handleLogin = async (event) => {
@@ -58,29 +89,62 @@ const App = () => {
     setUser(null)
   }
 
-  if (user === null) {
-    return (
-      <Form
-        handleLogin={handleLogin}
-        username={username}
-        changeHandler={changeHandler}
-        password={password}
-        passChangeHandler={passChangeHandler}
-        user={user}
-      />
-    )
-  }
-
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={form.title}
+          name="title"
+          onChange={updateAddBlogField}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          type="text"
+          value={form.author}
+          name="author"
+          onChange={updateAddBlogField}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="url"
+          value={form.url}
+          name="url"
+          onChange={updateAddBlogField}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
   return (
     <div>
-      <h2>blogs</h2>
-      <p>
-        {user.name} logged in <button onClick={handleLogout}>logout</button>
-      </p>
+      {user === null ? (
+        <Form
+          handleLogin={handleLogin}
+          username={username}
+          changeHandler={changeHandler}
+          password={password}
+          passChangeHandler={passChangeHandler}
+        />
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <p>
+            {user.name} logged in <button onClick={handleLogout}>logout</button>
+          </p>
+          <h2>Create new</h2>
+          {blogForm()}
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
